6c193ba8eb08d644b638892e61afef2b5f81872b
priyaaaansh <153048542+priyaaaansh@users.noreply.github.com>
2026-07-08 10:42:26 +0000

    dataTypes

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,36 @@
+// # Primitive Data Types
+// 7 types: string, number, boolean, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.4
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId)
+
+// const bigNumber = 3456145678765n
+// console.log(bigNumber)
+
+
+// ****************** Reference(Non primitive) ****************** 
+// Array, Objects, Functions
+
+// const heros = ["ironman", "spiderman", "hulk"];
+
+// let myObj = {
+//     name: "tony",
+//     age: 45,
+//     isAvenger: true
+// }
+
+// const myFunction = function(){
+//     console.log("hello world");
+// }
+
+
+
+
