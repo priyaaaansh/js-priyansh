17af34c019276847d4524b9439580794a85375e3
priyaaaansh <153048542+priyaaaansh@users.noreply.github.com>
2026-07-09 11:26:53 +0000

    arrays final

---
+++ 02_basics/02_array.js
@@ -0,0 +1,32 @@
+const marvel_heros = ["thor","Iron man","Spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allNewHeros = [...marvel_heros , ...dc_heros];
+// console.log(allNewHeros);
+
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log (real_another_array);
+
+
+// console.log(Array.isArray("Priyansh"))
+// console.log(Array.from("Priyansh"))
+
+// console.log(Array.from({name:"Priyansh"})) //Interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+
+console.log(Array.of(score1,score2,score3))
+
+
+
+
+
