c2049939d936a19cabd124647fa10105b19a7bf3
priyaaaansh <153048542+priyaaaansh@users.noreply.github.com>
2026-07-09 13:20:00 +0000

    objects part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,39 @@
+// const tinderUser = new Object()
+
+const tinderUser ={}
+tinderUser.id= "123abc"
+tinderUser.name= "abc"
+tinderUser.isLoggedIn= false
+
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:"sum@gmail.com",
+    fullName: {
+        userFullName:{
+            firstName: "Priyansh",
+            lastName: "Thapar"
+        }
+    }
+}
+// console.log(regularUser.fullName.userFullName.firstName)
+
+// console.log(regularUser.fullName) 
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj3 = Object.assign({}, obj1, obj2);      // add arrays easily 
+const obj4 = {...obj1,...obj2};
+
+
+// console.log(obj3);
+// console.log(obj4);
+
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
