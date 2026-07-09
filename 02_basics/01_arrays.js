7617b9d0c87452fc45497065db3a51e80c4b2790
priyaaaansh <153048542+priyaaaansh@users.noreply.github.com>
2026-07-09 10:59:29 +0000

    arrays pt 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,18 @@
+const myArr = [0,1,2,3,4,5]
+
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr2);
+// console.log(myArr[1]);
+
+myArr.push(6)
+console.log(myArr);
+
+
+myArr.pop()
+console.log(myArr)
+
+myArr.unshift(9)
+console.log(myArr);
+
+myArr.splice(1,3)
+console.log(myArr)
