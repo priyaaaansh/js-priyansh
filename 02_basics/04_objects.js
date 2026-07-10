7d80351376a9a84348a8928ca10b8a06250dd13c
priyaaaansh <153048542+priyaaaansh@users.noreply.github.com>
2026-07-10 05:11:59 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -31,9 +31,38 @@ const obj4 = {...obj1,...obj2};
 // console.log(obj4);
 
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+// ************************* DeStructuring *************************
+
+const course = {
+    courseName: "Priyansh ke pravachan",
+    price: "999",
+    courseInstructor: "Priyansh"
+};
+// course.courseInstructor
+
+const {courseInstructor}= course 
+// console.log(courseInstructor);
+
+
+// {
+//     "name" :"Priyansh",
+//     "couseName" : "wow pravachan",
+//     "price" :"free"
+// };
+
+
+[
+    {},
+    {},
+    {},
+    {}
+]
