6cec4abae02f2dd511f4e1b9c992a6b06ccb1c95
priyaaaansh <153048542+priyaaaansh@users.noreply.github.com>
2026-07-10 07:24:48 +0000

    arrow function

---
+++ 03_basics.js/03_arrow.js
@@ -0,0 +1,16 @@
+const user={
+    username: "Priyansh",
+    price: 999,
+
+    welcomeMessage : function(){
+        // console.log(`${this.username}, welcome to website`);
+    }
+
+}
+// user.welcomeMessage()
+
+
+// const addTwo = (num1,num2) =>  (num1 + num2) // implicit return 
+const addTwo = (num1,num2) => {username: "Priyansh"}
+
+console.log(addTwo(3,4))
